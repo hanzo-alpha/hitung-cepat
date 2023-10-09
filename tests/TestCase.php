a08582c70086576d4518254ac7809d503aebe3b1
Hansen Makangiras <hansenmakangiras@gmail.com>
2023-10-10 03:29:06 +0800

    Replace SQLite with MySQL in test settings and refactor setup
    Changed the database setup in TestCase.php and phpunit.xml from SQLite to MySQL for realistic test conditions. Move the environment specific variables to a new .env.testing file. Additionally removed use of RefreshDatabase trait in Pest.php due to its irrelevance in current setup.

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,7 +2,6 @@
 
 namespace Tests;
 
-use App\Models\User;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
@@ -11,21 +10,21 @@ abstract class TestCase extends BaseTestCase
     use CreatesApplication;
     use DatabaseTransactions;
 
-    protected function setUp(): void
-    {
-        parent::setUp();
-
-        $this->actingAs(User::role('super_admin')->first());
-
-        config()->set('database.default', 'testing');
-        config()->set('database.connections.testing', [
-            'driver' => 'mysql',
-            'host' => 'localhost',
-            'username' => 'root',
-            'password' => 'BlackID85',
-            'charset' => 'utf8mb4',
-            'collation' => 'utf8mb4_unicode_ci',
-            'database' => 'laravel_wilayah',
-        ]);
-    }
+    //    protected function setUp(): void
+    //    {
+    //        parent::setUp();
+    //
+    //        $this->actingAs(User::role('super_admin')->first());
+    //
+    //        config()->set('database.default', 'testing');
+    //        config()->set('database.connections.testing', [
+    //            'driver' => 'mysql',
+    //            'host' => 'localhost',
+    //            'username' => 'root',
+    //            'password' => 'BlackID85',
+    //            'charset' => 'utf8mb4',
+    //            'collation' => 'utf8mb4_unicode_ci',
+    //            'database' => 'hitung_cepat_testing',
+    //        ]);
+    //    }
 }
